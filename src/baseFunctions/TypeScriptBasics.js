af34c80ac9242b9b1b972b714129e40b1ca95143
Arun S V <arunsv2342001@gmail.com>
2026-03-26 10:34:06 +0530

    TypeScript Basics

---
+++ src/baseFunctions/TypeScriptBasics.js
@@ -0,0 +1,10 @@
+var TypeScriptBasics = /** @class */ (function () {
+    function TypeScriptBasics() {
+    }
+    TypeScriptBasics.prototype.greet = function () {
+        console.log("Welcome to TypeScript Class");
+    };
+    return TypeScriptBasics;
+}());
+var day1 = new TypeScriptBasics();
+day1.greet();
---
+++ src/baseFunctions/TypeScriptBasics.ts
@@ -0,0 +1,16 @@
+class TypeScriptBasics
+{
+    constructor()
+    {
+
+    }
+
+    greet()
+    {
+        console.log("Welcome to TypeScript Class");
+    }
+
+}
+
+let day1=new TypeScriptBasics();
+day1.greet();
